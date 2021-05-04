f31018c40e5d350e819974900ad26e83f508d5a8
Alejandro Fernández Trigo <alejandro@fertry.tech>
2021-05-04 13:34:32 +0200

    Ficheros práctica 8

---
+++ Práctica8/Ejercicio 1.asm
@@ -0,0 +1,16 @@
+﻿.config
+        readprotect off
+        writeprotect off
+        pipelined off
+        split off
+        cache  64 8 1 cbwa lru ; Config Caché de datos
+    
+.text 0x00401404
+        addi $2, $0, 0x0290    
+        addi $3, $2, 8
+        addi $5, $0, 0
+loop:   lb $10, 0($2)
+        add $5, $5,$10
+        addi $2,$2,1
+        slt $4, $2,$3
+        bne $4,$0, loop
